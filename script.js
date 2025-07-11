78fb876fdfbe61f5cb9f5db0f57ccf2dc00f1846
Sabar <sabarmasood764@gmail.com>
2025-07-11 09:09:05 +0100

    code files

---
+++ script.js
@@ -0,0 +1,9 @@
+const wrapper =document.querySelector('.wrapper');
+const loginLink =document.querySelector('.login-link');
+const registerLink= document.querySelector('.register-link');
+const btnpopup= document.querySelector('.btnlogin-popup');
+const closeicon= document.querySelector('.icon-close');
+registerLink.addEventListener('click',()=>{wrapper.classList.add('active');});
+loginLink.addEventListener('click',()=>{wrapper.classList.remove('active');});
+btnpopup.addEventListener('click',()=>{wrapper.classList.add('active-popup');});
+closeicon.addEventListener('click',()=>{wrapper.classList.remove('active-popup');});
